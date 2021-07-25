f1e1263b9ac7c60aa994e86e613152cda39064b3
Tyler Haddix <tyler.hadx@gmail.com>
2021-07-25 09:40:58 -0500

    first commit

---
+++ website/src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-card flat>
+    <v-card-title>The Search For Walter McDermitt</v-card-title>
+    <v-card-text>
+      <v-row>
+        <v-col>
+          Join one family as they recount the mischievous adventures of Leech Lakes most infamous walleye.
+
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+      <iframe src="https://cdn.flipsnack.com/widget/v2/widget.html?hash=dzh0x76f0g" width="100%" height="480" seamless="seamless" scrolling="no" frameBorder="0" allowFullScreen></iframe>
+        </v-col>
+      </v-row>
+    </v-card-text>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn href="https://www.peecho.com/print/en/1032982">Buy Print 12$(Discount Bulk Orders)</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  mounted:{
+
+  
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
