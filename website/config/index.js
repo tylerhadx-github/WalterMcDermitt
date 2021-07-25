913ffb660950bd5957764f4163f6e97849bee3d2
Tyler Haddix <tyler.hadx@gmail.com>
2021-07-25 10:23:14 -0500

    Update index.js

--- website/config/index.js
+++ website/config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/WalterMcDermitt/',
     proxyTable: {},
 
     // Various Dev Server settings
